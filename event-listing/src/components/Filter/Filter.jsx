6bc7dc359586e598c52645482d69651c392bf687
Rinat Valiullov <4rontender@gmail.com>
2021-11-01 18:32:06 +0500

    filter out duplicate data

--- event-listing/src/components/Filter/Filter.jsx
+++ event-listing/src/components/Filter/Filter.jsx
@@ -1,11 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import style from './Filter.module.css';
 import { Select } from '../UI/Select/Select';
 
 const Filter = ({ events }) => {
 
-  const getMonth = (date) => {
-    return new Date(date.split('.').reverse().join('-')).toLocaleDateString('en-US', { month: 'long' });
+  const getCities = (events) => {
+    return events
+      .map(event => event.city)
+      .filter((city, index, self) => self.indexOf(city) === index);
+  };
+
+  // const showMonth = (date) => {
+  //   return new Date(date.split('.').reverse().join('-')).toLocaleDateString('en-US', { month: 'long' });
+  // };
+
+  const getMonths = (events) => {
+    return events
+      .map(elem => {
+        return new Date(elem.date.split('.').reverse().join('-')).toLocaleDateString('en-US', { month: 'long' });
+      })
+      .filter((month, index, self) => self.indexOf(month) === index)
+      .map(date => ({ value: date }));
   };
 
   return (
@@ -13,16 +28,16 @@ const Filter = ({ events }) => {
       <Select
         label="City"
         onChange={() => 'city'}
-        options={events.map(event => ({ value: event.city }))}
+        options={
+          getCities(events).map(city => ({ value: city }))
+        }
       />
       <Select
         label="Month"
         onChange={() => 'month'}
-        options={events.map(event => (
-          {
-            value: getMonth(event.date)
-          }
-        ))}
+        options={
+          getMonths(events)
+        }
       />
     </div>
   );
